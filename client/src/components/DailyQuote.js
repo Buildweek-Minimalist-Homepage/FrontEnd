6281bc899759935912d46ebca6192838b360a1df
occludedarp <cmrnmr@gmail.com>
2019-11-20 14:25:42 -0500

    connected to backend

--- client/src/components/DailyQuote.js
+++ client/src/components/DailyQuote.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import styled from "styled-components";
-import { array } from 'prop-types';
+// import { array } from 'prop-types';
 
 const Container1 = styled.div`
 textAlign: "center";
 
 `
 const QuoteCard = styled.h2`
-color: "#3AAFA9";
+color: "grey";
 `
 
 const DailyQuote = () => {
@@ -17,17 +17,6 @@ const DailyQuote = () => {
 
   const [authorIs, setAuthor] = useState('Some Guy');
 
-  // "Access-Control-Request-Headers": "Authorization",
-  // "Access-Control-Allow-Headers": "Authorization",
-  // "Access-Control-Allow-Origin": "http://localhost:3001",
-
-  // .get('https://api.paperquotes.com/apiv1/qod/', {
-  //   "Access-Control-Allow-Headers": "Authorization",
-  //   "Access-Control-Allow-Origin": "http://localhost:3001",
-  //   "headers": {
-  //     "Authorization": "Token fdefb43787380c16f601497a3671c8198bc19cb5"
-  //   } 
-  // })
   
   useEffect(()=> {
     
@@ -35,25 +24,22 @@ const DailyQuote = () => {
     axios
     .get('https://minimalist-homepage.herokuapp.com/api/quote')
     .then(response => {
-      console.log(response.data.results)
-      // let quotesArr = response.data.results;
-      // const randomQuote = () => 
-      //   quotesArr[Math.floor(Math.random() * quotesArr.length)];
-      // setQuote(randomQuote())
-    })
+      console.log(response.data)
+      setQuote(response.data.content)
+      setAuthor(response.data.author)
+      })
+    
     .catch(error => {
       console.log(error, 'we could not find the quote of the day')
     })
 
-  }, [quoteIs])
+  }, [])
 
   return(
-    <Container1>
-      <QuoteCard>
-      "{quoteIs}"
-      </QuoteCard>
-      <h3>-{authorIs}</h3>
-    </Container1>
+    <div>
+      <QuoteCard>"{quoteIs}"</QuoteCard>
+      <p>-{authorIs}</p>
+    </div>      
   );
 }
 
